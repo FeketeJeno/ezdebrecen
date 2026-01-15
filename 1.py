3f7f8ccca4c3e9c826a580c7308072a625cae6dc
Fekete Jenő <feketej@kkszki.hu>
2026-01-15 18:19:36 +0100

    szam bekérés

---
+++ 1.py
@@ -0,0 +1,5 @@
+"""1. feladat – Szelekció (if-elif-else)
+Feladat: Írj egy programot, ami bekér egy egész számot, majd kiírja, hogy pozitív páros, pozitív
+páratlan, negatív páros, negatív páratlan, vagy nulla."""
+
+szam=int(input("Kérem egy egész számot: "))
